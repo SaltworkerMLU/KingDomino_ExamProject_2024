c1afca557393172ec7138cae4670ef97dd23fa05
Mathias Lykholt-Ustrup <saltworkermlu@gmail.com>
2024-10-15 18:14:20 +0200

    Update Kingdomino.py

--- Kingdomino.py
+++ Kingdomino.py
@@ -6,26 +6,21 @@ import glob
 from crowns_array import scorecard
 
 """
-Initialize stuff
-"""
-
-
-"""
 Get colors of each tile from Kingdomino game
 """
 def Kingdomino_colorEvaluate(img):
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img_canny = cv2.Canny(img, 50, 75) 
 
     # Apply thresholding to get colors of tiles
     _, img_blue = cv2.threshold(img[:,:,0], 100, 255, cv2.THRESH_BINARY)
     _, img_yellow = cv2.threshold(img[:,:,2], 175, 255, cv2.THRESH_BINARY) # try 160 # ALT: derive from max value of red
     _, img_forest = cv2.threshold(img[:,:,2] - img[:,:,1], 225, 255, cv2.THRESH_BINARY)
-    _, img_greenR = cv2.threshold(img[:,:,2], 127, 255, cv2.THRESH_BINARY)
+    _, img_greenR = cv2.threshold(img[:,:,2], 127, 255, cv2.THRESH_BINARY) # 127
     _, img_greenG = cv2.threshold(img[:,:,1], 127, 255, cv2.THRESH_BINARY)
     img_green = img_greenG - img_greenR
     _, img_mines = cv2.threshold(img_gray, 50, 255, cv2.THRESH_BINARY_INV)
     _, img_desert = cv2.threshold(img[:,:,0], 30, 255, cv2.THRESH_BINARY)
-    img_canny = cv2.Canny(img, 50, 100)
 
     castleMatch = np.empty(25)
     # For every tile in presumed 5x5 grid on 500x500 image
@@ -52,48 +47,29 @@ def Kingdomino_colorEvaluate(img):
         # 6. desert - Contains blue tiles AND CASTLE!!!
         if yellowMatch > 0.25:
             score_colors[tile] = 'Y'
+            cv2.rectangle(img, (tileColumn+40, tileRow+40), (tileColumn+60, tileRow+60), (0, 255, 255), -1)
         elif blueMatch > 0.5:
             score_colors[tile] = 'B'
+            cv2.rectangle(img, (tileColumn+40, tileRow+40), (tileColumn+60, tileRow+60), (255, 0, 0), -1)
         elif forestMatch > 0.5:
             score_colors[tile] = 'F'
+            cv2.rectangle(img, (tileColumn+40, tileRow+40), (tileColumn+60, tileRow+60), (0, 127, 0), -1)
         elif greenMatch > 0.25:
             score_colors[tile] = 'G'
+            cv2.rectangle(img, (tileColumn+40, tileRow+40), (tileColumn+60, tileRow+60), (0, 255, 0), -1)
         elif minesMatch > 0.5:
             score_colors[tile] = 'M'
+            cv2.rectangle(img, (tileColumn+40, tileRow+40), (tileColumn+60, tileRow+60), (0, 0, 0), -1)
         elif desertMatch > 0.5:
             score_colors[tile] = 'D'
+            cv2.rectangle(img, (tileColumn+40, tileRow+40), (tileColumn+60, tileRow+60), (127, 127, 127), -1)
 
 
     # Get Castle tile using canny edge detection
     # Method is not 100% perfect, castles placed on top of tile causes errors (sometimes)
-    score_colors[np.argmax(castleMatch)] = 'C'
-
-    # Visualize result on original image
-    # Yellow 'Y'
-    # Blue 'B'
-    # Dark green 'F'
-    # Green 'G'
-    # Black 'M'
-    # Grey 'D'
-    # White 'C'
-    for tile in range(25):
-        tileRow = (tile//5)*100
-        tileColumn = (tile%5)*100
-
-        if score_colors[tile] == 'Y':
-            cv2.rectangle(img, (tileColumn+40, tileRow+40), (tileColumn+60, tileRow+60), (0, 255, 255), -1)
-        elif score_colors[tile] == 'B':
-            cv2.rectangle(img, (tileColumn+40, tileRow+40), (tileColumn+60, tileRow+60), (255, 0, 0), -1)
-        elif score_colors[tile] == 'F':
-            cv2.rectangle(img, (tileColumn+40, tileRow+40), (tileColumn+60, tileRow+60), (0, 127, 0), -1)
-        elif score_colors[tile] == 'G':
-            cv2.rectangle(img, (tileColumn+40, tileRow+40), (tileColumn+60, tileRow+60), (0, 255, 0), -1)
-        elif score_colors[tile] == 'M':
-            cv2.rectangle(img, (tileColumn+40, tileRow+40), (tileColumn+60, tileRow+60), (0, 0, 0), -1)
-        elif score_colors[tile] == 'D':
-            cv2.rectangle(img, (tileColumn+40, tileRow+40), (tileColumn+60, tileRow+60), (127, 127, 127), -1)
-        elif score_colors[tile] == 'C':
-            cv2.rectangle(img, (tileColumn+40, tileRow+40), (tileColumn+60, tileRow+60), (255, 255, 255), -1)
+    castle_tile = np.argmax(castleMatch)
+    score_colors[castle_tile] = 'C'
+    cv2.rectangle(img, ((castle_tile%5)*100+40, (castle_tile//5)*100+40), ((castle_tile%5)*100+60, (castle_tile//5)*100+60), (255, 255, 255), -1)
 
 
 """
@@ -111,38 +87,39 @@ def Kingdomino_crownEvaluate(img, template_crown):
         
             img_template[y, x] = pixel
 
+    img_template = cv2.cvtColor(img_template, cv2.COLOR_BGR2GRAY)
     pts = []
 
     for i in range(4):
         # Use template matching to find crowns in each orientation
         patternMatch = cv2.matchTemplate(img_template, template_crown, cv2.TM_CCOEFF_NORMED)
-        patternMatch = cv2.cvtColor(patternMatch, cv2.IMREAD_GRAYSCALE)
         _, img_crowns = cv2.threshold(patternMatch, 0.5, 255, cv2.THRESH_BINARY)
-        img_crowns = cv2.cvtColor(img_crowns, cv2.COLOR_BGR2GRAY)
 
+        # Rotate template 90 degrees
         template_crown = cv2.rotate(template_crown, cv2.ROTATE_90_CLOCKWISE)
+        w, h = template_crown.shape
 
-        # Draw rectangles around the matched regions
+        # Get coordinates of each pixel with HIGH value
         locations = np.where(img_crowns >= 0.5)
-        w, h, RGB = template_crown.shape
 
         for pt in zip(*locations[::-1]):  # Switch columns and rows
-            if len(pts) == 0:
-                pts.append(pt)
-            # Draw rectangle on the original image
-            rect = np.array([(pt[0], pt[1]), (pt[0] + w, pt[1] + h)])
-            cv2.rectangle(img, rect[0], rect[1], (0, 0, 255), 1)
             pts.append(pt)
 
-            # Each point 
+            # Draw rectangle on the original image
+            cv2.rectangle(img, (pt[0], pt[1]), (pt[0] + w, pt[1] + h), (0, 0, 255), 1)
+
+            # Each point is claustrophobic - remove points to near to prior registered points
             for i in range(len(pts)-1):
                 if pt[0] > pts[i][0] - w//2 and pt[0] < pts[i][0] + w//2 and pt[1] > pts[i][1] - h//2 and pt[1] < pts[i][1] + h//2:
                     pts.pop() # Get rid of point
                     break
-
+            
     # Add each point - pixel - from pts[] as an additional crown to specified tile
     for i in range(len(pts)):
-        pt_score_index = (pts[i][0]//100) + (pts[i][1]//100)*5
+        # Get center point of given point
+        pt_center = [(pts[i][0]+w//2), (pts[i][1]+h//2)]
+
+        pt_score_index = (pt_center[0]//100) + (pt_center[1]//100)*5
         tem = ord(score_crowns[pt_score_index])
         score_crowns[pt_score_index] = chr(tem+1)
 
@@ -161,8 +138,6 @@ Using the following logic:
 'C' = castle
 """
 def kingdomino_scoreCalculator(game_colors, game_crowns):
-    game_colors = np.array(game_colors)
-    game_crowns = np.array(game_crowns)
     score = 0
 
     # A 5x5 grid always contains 25 tiles. 
@@ -192,7 +167,6 @@ def kingdomino_scoreCalculator(game_colors, game_crowns):
             adjacent_colors.append(game_colors[current])
             if game_crowns[current] != '':
                 adjacent_crowns += int(game_crowns[current])
-            
 
             # Check if neighboring tiles have same color as current entry
             if game_colors[current] == game_colors[current-5] and current-5 > 0 and game_colors[current-5] != '':
@@ -218,7 +192,7 @@ def kingdomino_scoreCalculator(game_colors, game_crowns):
 # Extract files from each of the folders containing dominos, castles, and games
 game_paths = sorted(glob.glob("Cropped and perspective corrected boards/*.jpg"))
 
-template_crown = cv2.imread("crown.jpg")
+template_crown = cv2.imread("crown.jpg", cv2.IMREAD_GRAYSCALE)
 
 # [file, [x, y, RGB]]
 game = []
@@ -230,7 +204,7 @@ for i in range(len(game_paths)):
 
 
 # Evaluate each game one at a time
-for i in range(len(game)):
+for i in range(len(game)): # len(game)
     # np.copy() is used to store duplicates of numpy array in memory
     img_original = np.copy(game[i])
     img = np.copy(game[i])
